2b1ea84c9431d6ce022eb4726011a197aa79656b
Md. Masud <jamdmasud@gmail.com>
2017-12-16 21:14:04 +0600

    Assign teacher 2nd part

---
+++ src/app/Models/CourseAssign.ts
@@ -0,0 +1,9 @@
+export class CourseAssign{
+    public TeacherId:string;
+    public TeacherName: string;
+    public CourseId:string;
+    public CourseName:string;
+    public SectionId:string;
+    public SectionName:string;
+    public Semester: string;
+}
--- src/app/components/assign-course/assign-course.component.ts
+++ src/app/components/assign-course/assign-course.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl} from '@angular/forms';
-
+import {MatAutocompleteSelectedEvent} from '@angular/material/';
 import {Observable} from 'rxjs/Observable';
 import {startWith} from 'rxjs/operators/startWith';
 import {map} from 'rxjs/operators/map';
@@ -10,6 +10,7 @@ import { CourseServiceService } from '../../Services/data-services/course-servic
 import { SectionServicesService} from '../../Services/data-services/section-services.service';
 import {ToastyService, ToastyConfig, ToastOptions, ToastData} from 'ng2-toasty';
 import { AssignValidation } from '../../Models/AssignValidation'
+import { CourseAssign } from '../../Models/CourseAssign'
 import 'rxjs/add/operator/startWith';
 import 'rxjs/add/operator/map';
 
@@ -23,7 +24,8 @@ export class State {
   styleUrls: ['./assign-course.component.css']
 })
 export class AssignCourseComponent implements OnInit {
-  
+  counts:number = 0;
+  tCount:number = 0;
   teacherCtrl: FormControl;
   courseCtrl:FormControl;
   sectionCtrl: FormControl;
@@ -37,12 +39,18 @@ export class AssignCourseComponent implements OnInit {
   SelectedCourse: any;
   Sections:any[];
   validation: AssignValidation;
+  selectedSectionId:string;
+  private courseAssign: CourseAssign;
+  SelectedCourseList:CourseAssign[];
+  selected:any;
 
   constructor(private toastyService:ToastyService, private toastyConfig: ToastyConfig,private teacherData: TeacherDataServicesService, private courseService: CourseServiceService, private sectionService: SectionServicesService) {
     this.teacherCtrl = new FormControl();
     this.courseCtrl = new FormControl();
     this.sectionCtrl = new FormControl();
     this.validation = new AssignValidation();
+    this.courseAssign =  new CourseAssign();
+    this.SelectedCourseList = [];
   }
 
   ngOnInit() {
@@ -62,8 +70,7 @@ export class AssignCourseComponent implements OnInit {
 
   getAllCourse(){
     this.courseService.getAllCourse().subscribe(response => {
-      this.courses = response;
-      console.log(this.courses);
+      this.courses = response; 
       this.filteredCourse = this.courseCtrl.valueChanges.
         startWith(null).
         map(state => state ? this.filterCourse(state) : this.courses.slice())
@@ -81,19 +88,25 @@ export class AssignCourseComponent implements OnInit {
   }
 
   SelectTeacher(teacher){
-    this.SelectedTeacher = teacher;
-    this.takenCredit =  this.SelectedTeacher.TakenCredit;
-    this.remainingCredit = this.SelectedTeacher.RemainingCredit;
-
-    //Load courses for autocomplete after selecting teacher
-    this.getAllCourse();
+    if(this.tCount == 0){
+      this.SelectedTeacher = teacher;
+      this.takenCredit =  this.SelectedTeacher.TakenCredit;
+      this.remainingCredit = this.SelectedTeacher.RemainingCredit;
+      this.remainingCredit -= 2;
+      //Load courses for autocomplete after selecting teacher
+      this.getAllCourse();
+      this.tCount = 1;
+    }
   }
 
   SelectdCourse(course){
-    this.SelectedCourse = course;
-    console.log(this.SelectedCourse);
-    this.getSectionByBatchId(this.SelectedCourse.BatchId);
+    if(this.counts == 0){
+      this.SelectedCourse = course;  
+      this.getSectionByBatchId(this.SelectedCourse.BatchId);
+      this.counts = 1;
+    }
   }
+
 
   SelectdSection(sec){
     this.validation.CourseId = this.SelectedCourse.Id;
@@ -135,4 +148,33 @@ export class AssignCourseComponent implements OnInit {
       });
     });
   }
+  
+  assignCourse():void{
+    let credit = this.remainingCredit;
+    let teacherName = this.SelectedTeacher.Name;
+    this.remainingCredit -= this.SelectedCourse.CreditPerSection;
+    let section = this.Sections.find(x => x.Id == this.selectedSectionId) 
+    this.courseAssign.CourseId = this.SelectedCourse.Id;
+    this.courseAssign.CourseName = this.SelectedCourse.Name;
+    this.courseAssign.SectionName = section.Name;
+    this.courseAssign.SectionId = section.Id;
+    this.courseAssign.TeacherId = this.SelectedTeacher.Id;
+    this.courseAssign.TeacherName = teacherName;
+    this.courseAssign.Semester = this.SelectedCourse.Batch; 
+    //console.log(this.SelectedCourse)
+    if(this.remainingCredit < 0)
+    {
+      alert("You can't assign more than " + credit + " Credit to " +   teacherName);
+      this.remainingCredit = credit;
+    }else{
+      this.SelectedCourseList.push(this.courseAssign);
+      this.counts = 0;
+      this.tCount = 0;
+    }
+    console.log(this.SelectedCourseList);
+    this.courseAssign = new CourseAssign();
+
+    this.courseCtrl.setValue("")
+  }
+
 }
